73aa15803aeeab898576d68dd117a891f58ff84c
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2022-06-04 14:05:29 +0200

    created Section component

---
+++ components/Templates/Section.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+
+type SectionProps = {
+  title: string;
+  children: ReactNode;
+  className?: string;
+  id: string;
+};
+
+function Section({ title, children, className, id }: SectionProps) {
+  return (
+    <section className={`text-center px-4 ${className}`} id={id}>
+      <h3 className="text-4xl font-extrabold">{title}</h3>
+      {children}
+    </section>
+  );
+}
+
+export default Section;

d6fe912a2873074a56fe4dd219d9a8515328afb6
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2022-06-06 14:39:33 +0200

    added section classname for scrollspy

--- components/Templates/Section.tsx
+++ components/Templates/Section.tsx
@@ -9,7 +9,7 @@ type SectionProps = {
 
 function Section({ title, children, className, id }: SectionProps) {
   return (
-    <section className={`text-center px-4 ${className}`} id={id}>
+    <section className={`text-center px-4 ${className} section`} id={id}>
       <h3 className="text-4xl font-extrabold">{title}</h3>
       {children}
     </section>

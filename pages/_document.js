23bee8f92b2acc92818216612467b96e12133b47
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2022-06-06 15:34:35 +0200

    font optimization

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
             rel="stylesheet"

a268ef6ed98afd43a1caf511b493ec27a7c2322e
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2022-10-04 15:58:03 +0200

    added i18next

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLanguage: 'en',
+  supportedLanguages: ['en', 'es'],
+  i18next: {
+    debug: true,
+    initImmediate: false,
+    ns: ['common', 'home', 'work', 'resume', 'contact'],
+    defaultNS: 'common',
+    backend: {
+      loadPath: './src/locales/{{lng}}/{{ns}}.json', // will look for json files inside language folders
+    },
+  },
+  i18nextPlugins: { fsBackend: 'i18next-fs-backend' },
+}

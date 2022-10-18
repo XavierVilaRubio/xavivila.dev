0b1277681d115103f9116b965fc84f8994758ee5
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2022-10-18 14:57:30 +0200

    Started work section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import astroI18next from 'astro-i18next'
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next()],
+  vite: {
+    ssr: {
+      external: ['svgo'],
+    },
+  },
 })

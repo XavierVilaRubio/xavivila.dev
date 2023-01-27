0ac644b450a6c15b22e5da7a9782f0b95b3fa90a
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2023-01-27 18:07:34 +0100

    added mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config'
-import astroI18next from 'astro-i18next'
+import { defineConfig } from 'astro/config';
+import astroI18next from 'astro-i18next';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  integrations: [astroI18next(), mdx()],
   vite: {
     ssr: {
-      external: ['svgo'],
-    },
-  },
-})
+      external: ['svgo']
+    }
+  }
+});
